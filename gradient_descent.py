af8c436b9f5081ffce240101bbc678517fd215f3
ChaitanyaAwasthi <124452335+ChaitanyaAwasthi@users.noreply.github.com>
2023-03-24 13:32:59 +0530

    Add files via upload

---
+++ gradient_descent.py
@@ -0,0 +1,31 @@
+import numpy as np 
+
+def gradient_descent(x,y):
+    m_curr = b_curr = 0 
+    iterations = 10000 
+    n = len(x)
+    learning_rate = 0.001
+
+    for i in range(iterations):
+        y_predicted = m_curr* x + b_curr
+        cost = (1/n)*sum([val**2 for val in (y-y_predicted)])
+        md = -(2/n)*sum(x*(y-y_predicted)) #partial derivative
+        bd = -(2/n)*sum(y-y_predicted) #partial derivative
+        m_curr = m_curr - learning_rate *md #learning step
+        b_curr = b_curr - learning_rate *bd #learning step
+        print("m {}, b {}, cost {} iteration {}".format(m_curr, b_curr,cost,i)) #This line of code is using the format method to create a string that includes the values of three variables m_curr, b_curr, and i in a specific format.
+    
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([6, 7, 9, 11, 13]) 
+
+#we had these values, we had to use these values to create an equation out of it 
+
+gradient_descent(x,y)
+
+
+
+
+
+
+
+
